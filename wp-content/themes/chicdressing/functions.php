8178ef2c1872279ad493c4ad20bf7ea3a2cf538e
Pierre Bultez <pierre.bultez@proton.me>
2024-06-11 18:12:23 +0200

    autoptimize + polices local

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -5,4 +5,16 @@ function chicdressing_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
 }
 
+function dequeue_google_fonts() { 
+    wp_dequeue_style( 'ashe-playfair-font' ); 
+    wp_deregister_style( 'ashe-playfair-font' ); 
+    wp_dequeue_style( 'ashe-opensans-font' ); 
+    wp_deregister_style( 'ashe-opensans-font' ); 
+    wp_dequeue_style( 'ashe-kalam-font' ); 
+    wp_deregister_style( 'ashe-kalam-font' ); 
+    wp_dequeue_style( 'ashe-rokkitt-font' ); 
+    wp_deregister_style( 'ashe-rokkitt-font' ); 
+    } 
+    add_action( 'wp_enqueue_scripts', 'dequeue_google_fonts', 20 );
+
 ?>
